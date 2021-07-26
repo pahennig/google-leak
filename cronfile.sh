89dee1d8f2c2a593bcd6b0d278caf15907890aeb
PRH <paulo.hennig@hypeflame.com.br>
2021-07-26 12:22:19 -0300

    Adding cron file example

---
+++ cronfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SHELL=/bin/bash
+PWD=/root
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+# Ensure to change this variable according to the path where your base file is stored
+mydir=/home/ec2-user/blue-team/google-leak/
+
+docker run \
+    -h google-leak \
+	-v $mydir:/usr/app \
+	prh/google-leak unicorns 192.168.100.30 514
